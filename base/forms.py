6d0409f5b8374f7a3c0282850d8343656e8dcdbf
Dicken NGOLO <dickenmoungala@gmail.com>
2021-11-15 00:28:25 +0700

    Mobile Responsiveness

--- base/forms.py
+++ base/forms.py
@@ -3,6 +3,7 @@ from .models import Room
 from django.contrib.auth.models import User
 
 
+
 class RoomForm(ModelForm):
     class Meta:
         model = Room
@@ -15,3 +16,15 @@ class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+
+
+
+
+
+
+
+
+
+
+
--- base/views.py
+++ base/views.py
@@ -52,7 +52,7 @@ def loginPage(request):
 
 def logoutUser(request):
     logout(request)
-    return redirect('home')
+    return redirect('homes')
 
 
 def registerPage(request):
@@ -75,13 +75,14 @@ def registerPage(request):
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(
-        Q(topic__name__icontains=q) |
-        Q(name__icontains=q) |
-        Q(description__icontains=q)
-        )
+    rooms = Room.objects.filter\
+            (
+            Q(topic__name__icontains=q) |
+            Q(name__icontains=q) |
+            Q(description__icontains=q)
+            )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     # room_messages = Message.objects.all()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
@@ -199,6 +200,7 @@ def deleteMessage(request, pk):
     return render(request, 'base/delete.html', {'obj': message})
 
 
+
 @login_required(login_url='login')
 def updateUser(request):
     user = request.user
@@ -211,3 +213,16 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
 
     return render(request, 'base/update-user.html', {'form': form})
+
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
+
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
